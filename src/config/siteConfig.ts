0ab65c570c083cd9e2b1502f725dc4af2b03461f
宇宙无敌板鸭酱 <1748605289@qq.com>
2026-08-03 22:36:48 +0800

    Update src/config/siteConfig.ts (via Pages CMS)

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -31,7 +31,7 @@ export const siteConfig: SiteConfig = {
 	// 主题色
 	themeColor: {
 		// 主题色的默认色相，范围从 0 到 360。例如：红色：0，青色：200，蓝绿色：250，粉色：345
-		hue: 165,
+		hue: 265,
 		// 默认模式："light" 亮色，"dark" 暗色，"system" 跟随系统
 		defaultMode: "system",
 	},
@@ -88,7 +88,7 @@ export const siteConfig: SiteConfig = {
 		// 导航栏图标和标题是否跟随主题色
 		followTheme: false,
 		// 导航栏是否固定在顶部并始终可见
-		stickyNavbar: true,
+		stickyNavbar: false,
 	},
 
 	// 站点开始日期，用于统计运行天数
