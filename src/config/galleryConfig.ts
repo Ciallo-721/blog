edfcd1be63ed725bec48bd5ddc32e3faad695ea4
宇宙无敌板鸭酱 <1748605289@qq.com>
2026-08-04 23:12:36 +0800

    Update src/config/galleryConfig.ts (via Pages CMS)

--- src/config/galleryConfig.ts
+++ src/config/galleryConfig.ts
@@ -17,11 +17,11 @@ export const galleryConfig: GalleryConfig = {
 		// 每添加一个数组项就相当于添加了一个相册，记得在 public/gallery/ 目录下创建对应的子目录并放入图片
 		{
 			id: "acg-images",
-			name: "可爱流萤",
-			description: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
-			location: "崩坏：星穹铁道",
+			name: "ACG images",
+			description: "Ciallo～(∠・ω< )⌒★",
+			location: "ACG",
 			date: "2026-01-01",
-			tags: ["崩坏星穹铁道", "流萤"],
+			tags: ["动漫", "游戏"],
 		},
 	],
 
