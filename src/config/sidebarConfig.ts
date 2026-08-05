e6beda3478fa68820f8fc086829f3d9a82677535
宇宙无敌板鸭酱 <1748605289@qq.com>
2026-08-05 15:30:17 +0800

    Update src/config/sidebarConfig.ts (via Pages CMS)

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -11,7 +11,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 	// left: 仅显示左侧边栏
 	// right: 仅显示右侧边栏
 	// both: 双侧边栏，1280px以上同时显示左右，769-1279px根据tabletSidebar配置显示其中一侧
-	position: "left",
+	position: "both",
 
 	// 平板端(769-1279px)显示哪侧侧边栏，仅position为both时生效
 	// left: 平板端显示左侧边栏
@@ -46,23 +46,13 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件位置
 			position: "top",
 			// 是否在文章详情页显示
-			showOnPostPage: false,
-		},
-		{
-			// 组件类型：时段问候实时时钟组件
-			type: "timeGreeting",
-			// 是否启用该组件
-			enable: true,
-			// 组件位置
-			position: "top",
-			// 是否在文章详情页显示
-  			showOnPostPage: true,
+			showOnPostPage: true,
 		},
 		{
 			// 组件类型：公告组件
 			type: "announcement",
 			// 是否启用该组件
-			enable: false,
+			enable: true,
 			// 组件位置
 			position: "top",
 			// 是否在文章详情页显示
@@ -108,22 +98,20 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 				collapseThreshold: 10,
 			},
 		},
-		{
-			// 组件类型：侧边栏目录组件（只在文章详情页显示）
-			type: "sidebarToc",
-			// 是否启用该组件
-			enable: true,
-			// 组件位置
-			position: "sticky",
-			// 是否在文章详情页显示
-			showOnPostPage: true,
-			// 是否在非文章详情页隐藏
-			hideOnNonPostPage: true,
-		},
 	],
 
 	// 右侧边栏组件配置列表
 	rightComponents: [
+		{
+			// 组件类型：时段问候实时时钟组件
+			type: "timeGreeting",
+			// 是否启用该组件
+			enable: true,
+			// 组件位置
+			position: "top",
+			// 是否在文章详情页显示
+  			showOnPostPage: true,
+		},
 		{
 			// 组件类型：最新动态组件
 			type: "dynamic",
@@ -172,7 +160,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件类型：日历组件
 			type: "calendar",
 			// 是否启用该组件
-			enable: false,
+			enable: true,
 			// 是否显示组件标题
 			showTitle: false,
 			// 组件位置
@@ -191,7 +179,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件类型：侧边栏目录组件（只在文章详情页显示）
 			type: "sidebarToc",
 			// 是否启用该组件
-			enable: false,
+			enable: true,
 			// 组件位置
 			position: "sticky",
 			// 是否在文章详情页显示
@@ -261,6 +249,14 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 	// 这些组件只在移动端(<768px)显示在页面底部，独立于左右侧边栏配置
 	mobileBottomComponents: [
 		{
+			// 组件类型：时段问候实时时钟组件
+			type: "timeGreeting",
+			// 是否启用该组件
+			enable: true,
+			// 是否在文章详情页显示
+  			showOnPostPage: false,
+		},
+		{
 			// 组件类型：用户资料组件
 			type: "profile",
 			// 是否启用该组件
@@ -321,7 +317,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件类型：站点统计组件
 			type: "stats",
 			// 是否启用该组件
-			enable: true,
+			enable: false,
 			// 是否在文章详情页显示
 			showOnPostPage: false,
 		},
